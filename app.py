bc0705f7fdd1a449d6940ce930fa63e5bcd772b2
Ed D <ed.dockery@gmail.com>
2019-06-01 23:13:19 -0400

    Removed non alphabetic entries for words via regex

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import tweepy
 import itertools
 import collections
 from flask import Flask, render_template, request
+import re
 
 '''
 put your twitter api credentials in local file twitter_credentials.py in the format
@@ -13,6 +14,7 @@ ACCESS_TOKEN_SECRET = ''
 '''
 
 app = Flask(__name__)
+
 
 def get_tweets(apitwitter, search_text, num_tweets):
 
@@ -28,11 +30,16 @@ def get_word_counts(tweetstext):
     # List of all words used as strings with duplicates for counting
     words = list(itertools.chain(*tweet_words))
 
+    # Use regex to filter strings containing non-alphabetic
+    regex = re.compile(r'^[a-z]*$')
+    just_words = list(filter(regex.search, words))
+
     # Get counts all all tweet words
-    word_counts = collections.Counter(words)
+    word_counts = collections.Counter(just_words)
 
     # Return top 10 tweet words
     return word_counts.most_common(10)
+
 
 @app.route('/')
 def index():
@@ -42,7 +49,6 @@ def index():
     auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    search_term = "investing"
     # Use 20 for Max Tweets to not exhaust rate limits. Change to 1000 as required in the specs before deployment
     max_tweets = 20
 
@@ -57,19 +63,12 @@ def index():
     search_term += " -filter:retweets"
     filtered_tweets = get_tweets(api, search_term, max_tweets)
 
-    # Print Returned Tweets
-    for index, tweet in enumerate(filtered_tweets):
-        print("Tweet Number: {} Tweet Text:{} ".format(index + 1, tweet))
-
     # Get word counts from returned tweets
     word_counts = get_word_counts(filtered_tweets)
 
-    # Print Returned Word Counts
-    for word, count in word_counts:
-        print("Word: {} Count:{}".format(word, count))
-
     # Pass Tweet Text and Word Counts to Front End Display
     return render_template('home.html', tweets=filtered_tweets, word_counts=word_counts)
+
 
 if __name__ == '__main__':
 
